4a5bc3f47a4dc218ac049a59e270295e64ad6e02
sebub <sebbemain@gmail.com>
2022-10-29 21:05:30 +0200

    Update handin3.c

--- src/handin3.c
+++ src/handin3.c
@@ -2,20 +2,21 @@
 #include <math.h>
 double g_x(double a);
 double h_x(double b);
-double get_sum(double h, int n, double (*funk)(double));
+double get_sum(double a, double h, int n, double (*funk)(double));
 double trap(double a, double b, int n, double (*test)(double));
 
 int main(void)
 {
-   double a = 0,b = M_PI, a2= -2,b2 = 2,u,o;
-   int v[] = {2,4,8,16,32,64,128}, n;
+   double a = 7,b = 8, a2= -2,b2 = 2,u,o;
+   int v[] = {2,4,8,16,32,64,128,256,512, 1024, 2048}, n;
 
-    for (int i = 0; i < 7; ++i) {
+
+    for (int i = 0; i < (sizeof(v)/sizeof(int)); ++i) {
         n = v[i];
         u = trap(a,b,n,g_x);
         o = trap(a2,b2,n,h_x);
-        printf("%lf\n",u);
-        printf("%lf\n",o);
+        printf("area of g(x) at %d intervals %lf\n",v[i], u);
+        printf("area of h(x) at %d intervals %lf\n",v[i], o);
     }
 }
 
@@ -30,18 +31,21 @@ double h_x(double b){
     return sqrt(4 - (pow(b-2,2)));
 }
 
-double get_sum(double h, int n, double (*funk)(double)){
+double get_sum(double a, double h, int n, double (*funk)(double)){
     double sum = 0;
     for (int i = 0; i < n; ++i) {
-        sum += funk(h*i);
+        sum += funk(a+h*i);
+        //printf("%lf\n",sum);
     }
+    //printf("%lf",sum);
+
     return sum;
 }
 
 double trap(double a, double b, int n, double (*test)(double)){
     double h = (b-a)/n;
-    double sum = get_sum(h, n, test);
-    return (h/2)*(test(a)+test(b) + 2 * (sum));
+    double sum = get_sum(a, h, n, test);
+    return (h/2)*(test(a)+test(b) + (2 * (sum)));
 
 
 }
